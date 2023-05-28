0c9e65ce4653404468380559890827a7a52bea91
wuyuelin <412523834@qq.com>
2023-05-29 00:01:09 +0800

    试用element plus

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { ref } from 'vue'
+const queryInput = ref('')
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+]
+// 方法
+const handleRowClick = () => {
+  console.log('click')
+}
+</script>
+
+<template>
+  <div class="table-box">
+    <!-- 标题 -->
+    <div class="title">
+      <h2>CRUD demo</h2>
+    </div>
+    <!-- 查询 -->
+    <div class="query-box">
+      <el-input v-model="queryInput" placeholder="请输入姓名搜索" />
+      <el-button type="primary">增加</el-button>
+    </div>
+    <!-- 表格 -->
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column fixed prop="date" label="Date" width="150" />
+      <el-table-column prop="name" label="Name" width="120" />
+      <el-table-column prop="state" label="State" width="120" />
+      <el-table-column prop="city" label="City" width="120" />
+      <el-table-column prop="address" label="Address" width="600" />
+      <el-table-column prop="zip" label="Zip" width="120" />
+      <el-table-column fixed="right" label="Operations" width="120">
+        <template #default>
+          <el-button link type="primary" size="small" @click="handleRowClick">Detail</el-button>
+          <el-button link type="primary" size="small">Edit</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<style scoped>
+.table-box {
+  width: 800px;
+  position: absolute;
+  top: 25%;
+  /* left: 75%; */
+}
+</style>
